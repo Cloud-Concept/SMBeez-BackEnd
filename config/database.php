da05024211fd87efe599aaa254332d96982410ea
Hossam <hossam.struggler@gmail.com>
2018-04-04 00:23:01 +0200

    added database

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => 'testsmbeez.cbexrmhnuocd.us-east-2.rds.amazonaws.com',
+            'host' => 'localhost',
             'port' => env('DB_PORT', '3306'),
-            'database' => 'testsmbeez',
-            'username' => 'testsmbeez',
-            'password' => 'smbeez_database',
+            'database' => 'smbeez_database',
+            'username' => 'root',
+            'password' => '',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
