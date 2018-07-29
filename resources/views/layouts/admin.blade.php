41f2572becd8f2b99f94514028f83a2f0ca1d5fe
Hossam <hossam.struggler@gmail.com>
2018-07-29 17:01:02 +0200

    fixes

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="robots" content="noindex,nofollow">
         <link rel="icon" href="{{asset('favicon.ico')}}" type="image/x-icon" />
+        
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
